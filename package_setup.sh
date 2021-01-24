788069a971e7ce84f1e1c6b3939edace65886fb0
Liam Packer <liampacker@gmail.com>
2021-01-23 18:14:10 -0800

    dotfiles initial commit

---
+++ package_setup.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+mkdir -p $HOME/bin
+
+export PATH=$HOME/bin:$PATH
+
+# good packages :)
+sudo apt install -y \
+     build-essential \
+     clang-tools \
+     vim \
+     docker \
+     wget \
+     curl \
+     cmake \
+     autoconf \
+     sbcl \
+     chezscheme \
+     bash-completion \
+     rsync
+
+# miniconda install
+if [ ! -x "$(which conda)" ]; then
+    cd /tmp \
+        && wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+        && bash Miniconda3-latest-Linux-x86_64.sh
+fi 
+
+# rust install
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+## quicklisp setup
+# curl -O https://beta.quicklisp.org/quicklisp.lisp
+# sbcl --load quicklisp.lisp
+
+# julia install
+if [ ! -x "$(which julia)" ] ; then
+    cd /tmp/ \
+        && wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.3-linux-x86_64.tar.gz \
+        && tar -xvf julia-1.5.3-linux-x86_64.tar.gz \
+        && mv julia-1.5.3-linux-x86_64 $HOME/bin/
+fi 
+
+# install emacs27
+if [ ! -x "$(which emacs)" ] ; then
+    cd /tmp/ \
+        && git clone https://git.savannah.gnu.org/cgit/emacs.git \
+        && cd emacs \
+        && git checkout emacs-27 \
+        && ./autogen.sh \
+        && ./configure \
+        && make \
+        && sudo make install
+fi
---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+cd "$(dirname ${BASH_SOURCE})";
+
+git pull origin main
+
+function doIt() {
+    rsync --exclude ".git/" \
+          --exclude "setup.sh" \
+          --exclude "package_setup.sh" \
+          --exclude "README.md" \
+          --exclude ".emacs.d/" \
+          -avh --no-perms . ~;
+    ln -s .emacs.d $HOME/.emacs.d
+    source ~/.bash_profile
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    doIt;
+else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo ;
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt;
+    fi;
+fi;
+unset doIt;
