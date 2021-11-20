e4c4988efdd5e2c398ee27ed99560614b83b574a
Liam Packer <liampacker@gmail.com>
2021-11-19 21:07:30 -0500

    add akku and chez installation, add automatic APOD background update on i3 startup

--- package_setup.sh
+++ package_setup.sh
@@ -18,13 +18,15 @@ sudo apt install -y \
      tmux \
      tree \
      htop \
-     git
+     git \
+     i3 \
+     redshift
 
 # miniconda install
 # if [ ! -x "$(which conda)" ]; then
 #    cd /tmp \
-#        && wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-#        && bash Miniconda3-latest-Linux-x86_64.sh
+    #        && wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+    #        && bash Miniconda3-latest-Linux-x86_64.sh
 # fi
 
 ## quicklisp setup
@@ -34,9 +36,9 @@ sudo apt install -y \
 # julia install
 # if [ ! -x "$(which julia)" ] ; then
 #    cd /tmp/ \
-#        && wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.3-linux-x86_64.tar.gz \
-#        && tar -xvf julia-1.5.3-linux-x86_64.tar.gz \
-#        && mv julia-1.5.3 $HOME/
+    #        && wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.3-linux-x86_64.tar.gz \
+    #        && tar -xvf julia-1.5.3-linux-x86_64.tar.gz \
+    #        && mv julia-1.5.3 $HOME/
 # fi
 
 # install emacs28
@@ -57,8 +59,26 @@ fi
 if [ ! -x "$(which rg)" ] ; then
     # ripgrep install
     cd /tmp \
-       && curl -LO https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb \
-       && sudo dpkg -i ripgrep_12.1.1_amd64.deb \
-       && sudo apt update \
-       && sudo apt install ripgrep
+        && curl -LO https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb \
+        && sudo dpkg -i ripgrep_12.1.1_amd64.deb \
+        && sudo apt update \
+        && sudo apt install ripgrep
+fi
+
+# akku install
+if [ "" ] ; then
+    sudo apt install -Y guile-3.0 guile-3.0-dev guile-3.0-libs libcurl-dev
+    cd /tmp/\
+        && wget https://gitlab.com/akkuscm/akku/uploads/819fd1f988c6af5e7df0dfa70aa3d3fe/akku-1.1.0.tar.gz\
+        && tar -xvf akku-1.1.0.tar.gz\
+        && cd akku-1.1.0 \
+        && ./configure \
+        && make -j8 \
+        && sudo make install
+fi
+
+if [ "" ]; then
+    cd /tmp/ \
+        && git clone https://github.com/cisco/ChezScheme.git
+            && ./configure && sudo make install -j4
 fi
